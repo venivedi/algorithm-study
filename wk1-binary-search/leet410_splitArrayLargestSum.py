4b777f118c45619bb8f90f43f1d275f666cc9385
WangDafang <wonderful.register+github@gmail.com>
2019-04-08 01:37:43 -0400

    week1 homework finished

---
+++ wk1-binary-search/leet410_splitArrayLargestSum.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 01:10:24 2019
+
+@author: dwang
+"""
+
+class Solution(object):
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        def valid(mid):
+            cnt = 0
+            current = 0
+            for n in nums:
+                current += n
+                if current>mid:
+                    cnt += 1
+                    if cnt>=m:
+                        return False
+                    current = n
+            return True
+
+        l = max(nums)
+        h = sum(nums)
+
+        while l<h:
+            mid = l+(h-l)/2
+            if valid(mid):
+                h = mid
+            else:
+                l = mid+1
+        return l
